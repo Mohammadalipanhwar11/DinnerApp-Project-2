07a310f69b78c494d499a6ed05d92a83fba427b6
Mohammad Ali Panhwar <panhwarali11@gmail.com>
2020-08-09 21:34:12 +0500

    modified the dinner.js file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Dinner from './Dinner.js';
 
 function App() {
   return (
-    <Dinner dishName="Chicken Karahi"/>
+    <div>
+      <Dinner dishName="Chicken Karahi" sweet="Custurd"/>
+      <Dinner dishName="Chicken Biryani" sweet="Custurd"/>
+    </div>
+    
   );
 }
 
--- src/Dinner.js
+++ src/Dinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Dinner(props){
     return (
         <div>
-            <h1>Today we have {props.dishName} in dinner</h1>
+            <h1>Today we have {props.dishName} in dinner and {props.sweet} in sweet</h1>
         </div>
     );
 }
